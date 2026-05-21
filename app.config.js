a9a3662f26f8a6a55a342fccb29eaf5b252ba763
Halil İbrahim Kes <kes@Halils-MacBook-Air.local>
2026-05-21 17:32:02 +0300

    .

--- app.config.js
+++ app.config.js
@@ -3,6 +3,15 @@ const appJson = require('./app.json');
 
 module.exports = {
   ...appJson.expo,
+  android: {
+    ...appJson.expo.android,
+    config: {
+      ...appJson.expo.android?.config,
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
   extra: {
     ...appJson.expo.extra,
     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -307,6 +307,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        ...StyleSheet.absoluteFillObject,
     },
     map: {
         ...StyleSheet.absoluteFillObject,
